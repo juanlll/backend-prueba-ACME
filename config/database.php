82501a309e9b3a9ff0c6a980bc1b508fc06791e2
juanvv <juan.vargasva@gmail.com>
2021-02-07 00:13:07 -0500

    commit initial for heroku

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsl'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
